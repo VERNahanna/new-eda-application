4d27488f313a15c474f775c56e85f7d88968fbc4
Mahmoud Elsayed <mahmoud.elsayed@simon-kucher.com>
2022-01-31 18:56:35 +0200

    solve the enviornment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiURL: '/edaapi/api/',
-  compareURL: '/compare/api/',
+  secondApiURL: '/edaapi/api/'
   // loginAPIURL: 'http://107.180.75.165:9067/api/Login/Comp'
 
   // loginAPIURL: 'http://196.46.22.220/APICompanyToken/api/Login/Comp'  // with CORS error
